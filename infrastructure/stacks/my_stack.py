5b0bb2be71570d752b0e38b9487874a3b0efa650
Harry Fletcher <harry.fletcher@metoffice.gov.uk>
2023-10-20 16:37:46 +0100

    Testing infrastructure deployment part 1!

--- infrastructure/stacks/my_stack.py
+++ infrastructure/stacks/my_stack.py
@@ -1,7 +1,13 @@
 from aws_cdk import (
+    Duration,
     Stack,
+    aws_sqs as sqs,
+    aws_s3 as s3,
+    RemovalPolicy,
+    aws_lambda,
 )
 from constructs import Construct
+from app_constants import NAME
 
 class MyStack(Stack):
 
@@ -9,3 +15,26 @@ class MyStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+
+        queue = sqs.Queue(
+            self,
+            f"MyStackQueue-{NAME}",
+            queue_name=f"my-queue-{NAME}",
+            visibility_timeout=Duration.seconds(60)
+        )
+
+        bucket = s3.Bucket(
+            self,
+            f"MyStackBucket-{NAME}",
+            bucket_name=f"my-bucket-{NAME}",
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
+        lambda_function = aws_lambda.Function(
+            self,
+            f"MyStackFunction-{NAME}",
+            function_name=f"my-function-{NAME}",
+            code=aws_lambda.Code.from_asset("src/lambda/queue_to_bucket_lambda"),
+            handler="main.lambda_handler",
+            runtime=aws_lambda.Runtime.PYTHON_3_10,
+        )
